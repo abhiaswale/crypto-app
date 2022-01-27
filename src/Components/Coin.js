1e1e2e5a17a3889a3ada25134b9bc18503f108a0
abhiaswale <aswaleabhishek8@gmail.com>
2022-01-27 19:54:55 +0530

    icons

--- src/Components/Coin.js
+++ src/Components/Coin.js
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 const Coin = (props) => {
   const navigate = useNavigate();
   return (
-    <div className="lg:w-8/12 lg:p-0 lg:text-base h-20 grid lg:grid-cols-7  lg:gap-x-4 items-center text-white border-b-2 border-white w-full text-xs gap-0 p-4 grid-cols-5">
+    <div className="lg:w-8/12 lg:p-0 lg:text-base h-20 grid lg:grid-cols-7 lg:gap-x-4 items-center text-white border-b-2 border-white w-full text-xs gap-0 p-4 grid-cols-5">
       <img
         src={props.icon}
         alt={props.name}
